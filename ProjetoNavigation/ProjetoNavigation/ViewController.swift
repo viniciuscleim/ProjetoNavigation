0cdbc891bdafc3c45110d8e78aec05529a785b45
ViniciusCleim <vinicius.cleim@gmail.com>
2022-10-11 15:40:44 -0300

    exibi uma modal de maneira 100% programatica

--- ProjetoNavigation/ProjetoNavigation/ViewController.swift
+++ ProjetoNavigation/ProjetoNavigation/ViewController.swift
@@ -14,9 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
+
     @IBAction func tappedButton(_ sender: UIButton) {
-       performSegue(withIdentifier: "goTela02", sender: nil)
+        
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController
+        present(vc ?? UIViewController(), animated: true)
+        
         
     }
     
